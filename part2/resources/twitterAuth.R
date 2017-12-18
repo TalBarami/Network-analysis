7daf114b6c764dac0f2dd402a0b15b2b5fdb6de5
TalBarami <talbaramii@gmail.com>
2017-12-18 13:22:27 +0200

    Ignore twitterAuth.R

--- part2/resources/twitterAuth.R
+++ part2/resources/twitterAuth.R
@@ -1,4 +1,4 @@
-consumer_key <- ""
-consumer_secret <- ""
-access_token <- ""
-access_secret <- ""
+consumer_key <- "3yei82Jfbu9vwc2wmEAHBleaM"
+consumer_secret <- "FBbi0Xlb4jnPcLQttdZ9GaTuCrIypVWAPvMWrlUQuvuEtz4KU8"
+access_token <- "4832958718-HeaQrK3JwpDs4NfJ3caeiEcan5TPAVKzFiX4h6Q"
+access_secret <- "B0HS8xxD0HNaTDawB79DF0DlRV6ROvbbk9jLjM45zLLUW"
